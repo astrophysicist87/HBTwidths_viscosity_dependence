e58c86f1c8ab7f81974ff5f3e9f637c64147156e
Christopher Plumberg <astrophysicist87@gmail.com>
2015-04-24 13:37:15 -0400

    Updated estiamte_sigmy.py to handle estimate using weighted averages

--- RESULTS/RESULTS_etaBYs_0.00/estimate_sigma.py
+++ RESULTS/RESULTS_etaBYs_0.00/estimate_sigma.py
@@ -30,49 +30,49 @@ Cavgdata = Cavgdata[:,1:4]
 runningsum = zeros(Cavgdata.shape)
 
 #read in information about all subensembles
-#print 'Reading in information about all subensembles...'
-#
-#multiplicitiesFile = homeDirectory + '/simulations/subensemble_nb2_indexfile.dat'
-#subensembleBinsAndEventIndices = loadtxt(multiplicitiesFile)[:,2:((Nev/nb) + 3)].reshape(M,nb,Nev/nb).astype(int)-1
+print 'Reading in information about all subensembles...'
+
+multiplicitiesFile = homeDirectory + '/simulations/subensemble_nb2_indexfile.dat'
+subensembleBinsAndEventIndices = loadtxt(multiplicitiesFile)[:,2:((Nev/nb) + 3)].reshape(M,nb,Nev/nb).astype(int)-1
 # subtract one to convert results file indices to array indices
-#
-#print 'Finished reading in multiplicities.'
-#
+
+print 'Finished reading in multiplicities.'
+
 #read in multiplicities and get weights...
-#eventByEventMultiplicities = zeros([Nev,nKT])
-#print 'Reading in multiplicities...'
-#
-#for event in xrange(int(Nev)):
-#	multiplicitiesFile = homeDirectory + '/results-%(ev)d/Multiplicities_vs_KT_cfs_0.dat' % {"ev": event + 1}
-#	eventByEventMultiplicities[event,:] = loadtxt(multiplicitiesFile)[:,1]
-#
-#print 'Finished reading in multiplicities.'
-#
-#relativeBinWeights = zeros(nb)
+eventByEventMultiplicities = zeros([Nev,nKT])
+print 'Reading in multiplicities...'
+
+for event in xrange(int(Nev)):
+	multiplicitiesFile = homeDirectory + '/results-%(ev)d/Multiplicities_vs_KT_cfs_0.dat' % {"ev": event + 1}
+	eventByEventMultiplicities[event,:] = loadtxt(multiplicitiesFile)[:,1]
+
+print 'Finished reading in multiplicities.'
+
+relativeBinWeights = zeros(nb)
 
 #now run through Cavg results for each binning/bin and compute sigma estimate
 for binningIteration in xrange(int(M)):
 	for binIndex in xrange(int(nb)):
 		filename = homeDirectory + '/simulations/nb%(nb)d_files/binning_%(binit)d/CavgHBTradii_cfs_binning%(binit)d_nb%(nb)d_bin%(binid)d_no_df.dat' % {"nb": nb, "binit": binningIteration, "binid": binIndex}
 		data = loadtxt(filename)[:,1:4]
-		#print 'data.shape = ', data.shape
-		#binMultiplicities = eventByEventMultiplicities[subensembleBinsAndEventIndices[binningIteration,binIndex,:],:]
-		#print 'binMultiplicities.shape = ', binMultiplicities.shape
-		#binWeights = get_wnk(binMultiplicities)
-		#print 'binWeights.shape = ', binWeights.shape
-		#Vn2 = get_Vnl(binWeights, 2)
-		#print 'Vn2 = ', Vn2
-		#relativeBinWeights[binIndex] = get_relativeBinWeight(binMultiplicities, eventByEventMultiplicities)
-		#print 'relativeBinWeights.shape = ', relativeBinWeights.shape
-		#runningsum += (relativeBinWeights[binIndex]*(data-Cavgdata)**2)/Vn2
-		runningsum += (data-Cavgdata)**2
+		print 'data.shape = ', data.shape
+		binMultiplicities = eventByEventMultiplicities[subensembleBinsAndEventIndices[binningIteration,binIndex,:],:]
+		print 'binMultiplicities.shape = ', binMultiplicities.shape
+		binWeights = get_wnk(binMultiplicities)
+		print 'binWeights.shape = ', binWeights.shape
+		Vn2 = get_Vnl(binWeights, 2)
+		print 'Vn2 = ', Vn2
+		relativeBinWeights[binIndex] = get_relativeBinWeight(binMultiplicities, eventByEventMultiplicities)
+		print 'relativeBinWeights.shape = ', relativeBinWeights.shape
+		runningsum += (relativeBinWeights[binIndex]*(data-Cavgdata)**2)/Vn2
+		#runningsum += (data-Cavgdata)**2
 		#print 'runningsum.shape = ', runningsum.shape
 	
-	#Vtildenb2 = get_Vnl(relativeBinWeights, 2)
+	Vtildenb2 = get_Vnl(relativeBinWeights, 2)
 	print 'Finished binning iteration = %(binit)d' % {"binit": binningIteration}
 
-#factor = 1./M
-factor = Nev / (M*nb*(nb-1.))
+factor = 1./M
+#factor = Nev / (M*nb*(nb-1.))
 absoluteResults = factor * runningsum
 relativeResults = sqrt(absoluteResults) / Cavgdata
 
